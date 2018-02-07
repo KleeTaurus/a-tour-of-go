c2bf285cf91591fe8ac5306f8d0ccb5e5d496426
Kevin Lee <lining@luojilab.com>
2018-02-07 13:40:00 +0800

    convert tab space from 2 to 4

--- imports.go
+++ imports.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
-
+	
 func main() {
-    fmt.Println("Now you have %g problems.", math.Sqrt(7))
+	fmt.Println("Now you have %g problems.", math.Sqrt(7))
 }
